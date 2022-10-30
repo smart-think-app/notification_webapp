30fe4ba10d4196836a8ce6317c84879081c5d0f6
smart-think-app <79594145+smart-think-app@users.noreply.github.com>
2022-10-30 15:22:35 +0700

    noti web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
-
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'notification-webapp';
+
+  initFcmToken() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyB18_nAFNwDXvPnI6s3LFyf0AsMzOAhqBc",
+      authDomain: "parking-4292e.firebaseapp.com",
+      projectId: "parking-4292e",
+      storageBucket: "parking-4292e.appspot.com",
+      messagingSenderId: "460591389535",
+      appId: "1:460591389535:web:90a4e9d6bf112f644f74ea",
+      measurementId: "G-1W0Z34FJW2"
+    };
+    const app = initializeApp(firebaseConfig);
+    const messaging = getMessaging(app);
+    getToken(messaging, {
+      vapidKey:
+        'BAYt0leKWWhorFaYTxlpulnp5SN6hh1UmB7GPPXE1LpkrDagWRejnlAVNjdpuJyZ7LEYhN06aOoEhHAuCNwr_xc',
+    })
+      .then((currentToken) => {
+        if (currentToken) {
+          console.log('fcm_token');
+          console.log(currentToken);
+        } else {
+          // Show permission request UI
+          console.log(
+            'No registration token available. Request permission to generate one.'
+          );
+          // ...
+        }
+      })
+      .catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+        // ...
+      });
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      // ...
+    });
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.initFcmToken();
+  }
 }
